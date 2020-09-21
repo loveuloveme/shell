9f818fd28d027c357b2b43704448f8d2542b5e7a
loveuloveme <olegpodkolzin@icloud.com>
2020-09-21 03:39:20 +0300

    Search fix

--- modules/search/search.sh
+++ modules/search/search.sh
@@ -5,11 +5,11 @@ source app/error.sh
 
 function search(){
 
-    local regex="$2";
+    regex="$2";
 
     for i in $(ls $1);
     do
-        ! dir_exist $1${i} && search $1${i}/
+        ! dir_exist $1${i} && search $1${i}/ $2
 
         ! file_exist $1${i} && grep "$regex" $1${i};
     done
