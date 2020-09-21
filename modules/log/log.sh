ef9b960cd54ce8d5c2985415a79f139f311bfcf4
root <root@localhost.localdomain>
2020-09-21 12:00:26 +0300

    LOG

--- modules/log/log.sh
+++ modules/log/log.sh
@@ -4,12 +4,6 @@ function log(){
 	local YELLOW="\033[1;33m"
 	local BLUE="\033[1;34m"
 	local RESET="\033[0m"
-
-	echo -e $YELLOW;
-	cat /var/log/anaconda/X.log | grep "(II)";
-
-	echo -e $BLUE;
-	cat /var/log/anaconda/X.log | grep "(WW)";
-
-	echo -e $RESET;
+	cat /var/log/anaconda/X.log | grep "(II)" | sed -E "/(II)/s//$(printf "$YELLOW Information $RESET")/";
+	cat /var/log/anaconda/X.log | grep "(WW)" | sed -E "s/(WW)/Warning/g";
 }
