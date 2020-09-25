d99bdc6e5e1bccc904a333bf27c005da4c08c476
root <root@localhost.localdomain>
2020-09-25 18:42:25 +0300

    rev

--- modules/reverse/reverse.sh
+++ modules/reverse/reverse.sh
@@ -7,6 +7,7 @@ function reverse(){
     [[ -z "$1" ]] && err "Source filename required" 16 && return;
     [[ -z "$2" ]] && err "Out filename required" 16 && return;
 
+    ! dir_exist $1 && err "Expected file" 10 && return;
     file_exist $1 && err "$1 not exist" 10 && return;
     ! file_exist $2 && err "$2 already exist" 12 && return;
     file_readble $1 && err "$1 can't read" 15 && return;
